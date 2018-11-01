4f8f78810476b5d8e1ac02015039a884318c1b8d
MaxBlaushild <blaushmild@gmail.com>
2018-11-01 13:20:47 -0400

    lp578/feat/profiles

---
+++ routes/profiles.js
@@ -0,0 +1,15 @@
+module.exports = Profile => async (req, res) => {
+  const { address } = req.params;
+
+  const profile = await Profile.findOne({
+    where : {
+      address: address.toLowerCase(),
+    },
+  });
+
+  if (profile) {
+    res.json(profile);
+  } else {
+    res.status(404).send('No profile found for that address.');
+  }
+};
